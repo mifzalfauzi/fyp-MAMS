dc2538fdee3800dc0a007e0ea5a5d260fedfd16d
MIFZALAQIL\mifza <mifzal.aqil@gmail.com>
2025-10-08 17:54:21 +0800

    new fux

--- chatbot/authentication/urls.py
+++ chatbot/authentication/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('register/', views.registerPage, name='register' ),
-    path('', views.loginPage, name='login' ),
-    path('logout/', views.logoutUser, name = 'logout')
-
-
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
