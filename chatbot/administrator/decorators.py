377b8098e4d4c1fca9f005074ec00759708ded37
MIFZALAQIL\mifza <mifzal.aqil@gmail.com>
2025-10-09 12:14:09 +0800

    recent

--- chatbot/administrator/decorators.py
+++ chatbot/administrator/decorators.py
@@ -5,7 +5,7 @@ def admin_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         # Check if the user is logged in and belongs to the "admin" group
-        if request.user.is_authenticated and request.user.groups.filter(name='admin').exists():
+        if request.user.is_authenticated and request.user.groups.filter(name='Admin').exists():
             # If the user is an admin, allow access to the view
             return view_func(request, *args, **kwargs)
         
